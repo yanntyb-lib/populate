0ba61022c1f127f6dbb5e2911c418444136e47c0
yanntyb <yanntyb.lbc@gmail.com>
2022-01-10 17:53:30 +0100

    edit check if R already setup

--- src/Model/Classes/Populate.php
+++ src/Model/Classes/Populate.php
@@ -23,7 +23,7 @@ class Populate{
 
     public static function setup(string $dbname, string $user, string $password){
         if(!self::$setupFlag){
-            if(!R::getDatabaseAdapter()->getDatabase()->isConnected()){
+            if(!R::getDatabaseAdapter()){
                 R::setup("mysql:host=localhost;dbname=$dbname","$user","$password");
             }
             self::$dbname = $dbname;
